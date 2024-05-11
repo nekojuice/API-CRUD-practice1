203b89313803ef7d11ff3f8ee598de707a28bb85
nekojuice <joseapple7@gmail.com>
2024-05-11 15:41:23 +0800

    refactor: IResultResponse 新增屬性

--- Account_book.API/Domain/Response/ResultResponse.cs
+++ Account_book.API/Domain/Response/ResultResponse.cs
@@ -2,8 +2,16 @@
 
 namespace Account_book.API.Domain.Response;
 
-public interface IResultResponse;
+public interface IResultResponse
+{
+    ReturnCodeEnum ReturnCode { get;  }
+    string ReturnMessage { get;  }
+    object? ReturnData { get; }
+}
 
 public record ResultResponse(ReturnCodeEnum ReturnCode = ReturnCodeEnum.Success, string ReturnMessage = "", object? ReturnData = null) : IResultResponse;
-public record ResultResponse<T>(ReturnCodeEnum ReturnCode = ReturnCodeEnum.Success, string ReturnMessage = "", T? ReturnData = default) : IResultResponse;
+public record ResultResponse<T>(ReturnCodeEnum ReturnCode = ReturnCodeEnum.Success, string ReturnMessage = "", T? ReturnData = default) : IResultResponse
+{
+    object? IResultResponse.ReturnData => ReturnData;
+}
 
